2dbafc519fcabce09db75dd33cff5f3e435b88d1
polychrom <poly.chrom@outlook.com>
2022-06-22 21:52:05 +0200

    check url param to match project data

--- src/app/portfolio-detail-page/portfolio-detail-page.component.ts
+++ src/app/portfolio-detail-page/portfolio-detail-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-portfolio-detail-page',
@@ -7,12 +8,30 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./portfolio-detail-page.component.scss'],
 })
 export class PortfolioDetailPageComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  public project: any;
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+    let id = route.snapshot.params;
+    console.log('url', id);
+
+    this.apiService.getData('test').subscribe((response) => {
+      this.project = response.project;
+
+      this.project.forEach((item: any) => {
+        console.log(item.project_dir);
+        console.log(route.snapshot.params);
+
+        if (item.project_dir === route.snapshot.params['id']) {
+          this.project = item;
+          console.log('FOUND', this.project);
+        }
+      });
+    });
+  }
 
   //public routeParams: any;
 
   ngOnInit(): void {
-    
     const routeParams = this.route.snapshot.paramMap;
     const projectIdFromRoute = routeParams.get('projectId');
     console.log(routeParams);
