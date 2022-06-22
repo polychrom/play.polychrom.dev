2e259cb34b3e49af0ef7e294ec282a1180ec2eaf
polychrom <poly.chrom@outlook.com>
2022-06-22 22:24:57 +0200

    adjusted styling for header

--- src/app/portfolio-detail-page/portfolio-detail-page.component.ts
+++ src/app/portfolio-detail-page/portfolio-detail-page.component.ts
@@ -10,30 +10,18 @@ import { ApiService } from '../api.service';
 export class PortfolioDetailPageComponent implements OnInit {
   public project: any;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {
-    let id = route.snapshot.params;
-    console.log('url', id);
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
+  ngOnInit(): void {
     this.apiService.getData('test').subscribe((response) => {
       this.project = response.project;
 
       this.project.forEach((item: any) => {
-        console.log(item.project_dir);
-        console.log(route.snapshot.params);
-
-        if (item.project_dir === route.snapshot.params['id']) {
+        if (item.project_dir === this.route.snapshot.params['id']) {
           this.project = item;
-          console.log('FOUND', this.project);
+          console.log(this.project);
         }
       });
     });
-  }
-
-  //public routeParams: any;
-
-  ngOnInit(): void {
-    const routeParams = this.route.snapshot.paramMap;
-    const projectIdFromRoute = routeParams.get('projectId');
-    console.log(routeParams);
   }
 }
