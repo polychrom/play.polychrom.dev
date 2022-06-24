9d31772a1abd27a3d488cda22f1b3be7ac0fd215
polychrom <poly.chrom@outlook.com>
2022-06-24 21:36:15 +0200

    added first projects to integrate images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Location, LocationStrategy } from '@angular/common';
 
 @Component({
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private locationStrategy: LocationStrategy) {}
+  public open = false;
+  constructor(
+    private locationStrategy: LocationStrategy,
+    private _eref: ElementRef
+  ) {}
+
+  onClick(event: { target: any }) {
+    console.log('element', this._eref.nativeElement);
+    if (!this._eref.nativeElement.contains(event.target))
+      this.open = false;
+      console.log('triggered');
+  }
 
   projectLink = 'project-a';
   baseUrl = this.locationStrategy.getBaseHref();
 
   ngOnInit(): void {}
+
+  openModal(): void {
+    this.open = true;
+  }
+
+  closeModal(): void {
+    this.open = false;
+  }
 }
