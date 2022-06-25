b86d3bbabfcc2f18d8a1afa9fac5db040f25a079
polychrom <poly.chrom@outlook.com>
2022-06-25 14:31:17 +0200

    show last viewed projects in navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { HelperService } from '../helper.service';
 import { SharedService } from '../shared.service';
 
 @Component({
@@ -8,14 +10,25 @@ import { SharedService } from '../shared.service';
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  public projectViews: any;
+  public projectsViewed: any;
   $navigationModal = new BehaviorSubject(123);
 
-  constructor(private sharedService: SharedService) {}
+  constructor(
+    private sharedService: SharedService,
+    private router: Router,
+    public helperService: HelperService
+  ) {}
 
   ngOnInit(): void {
     this.$navigationModal.next(789);
 
     this.$navigationModal.subscribe((val) => console.log('modal', val));
+
+    this.projectViews = sessionStorage.getItem('projectViews');
+    const viewed = sessionStorage.getItem('projectsViewed');
+    this.projectsViewed = viewed ? viewed.split(',') : [];
+    console.info('xxx', this.projectsViewed);
   }
 
   closeModal(): void {
