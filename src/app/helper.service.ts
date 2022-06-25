5f6d22fb00917b29749e82de49fae6def0dfc1b8
polychrom <poly.chrom@outlook.com>
2022-06-25 14:30:32 +0200

    added routing url function

--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HelperService {
-  constructor() {}
+  constructor(private router: Router) {}
 
   getWindowSize(): string {
     return 'hello from getWindowSize';
+  }
+
+  navigateByRoute(url: any): void {
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate([url]));
   }
 }
