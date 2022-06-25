a4b13fe01f183dec3e4144f0b46599e112522439
polychrom <poly.chrom@outlook.com>
2022-06-25 14:32:27 +0200

    show images and update view count

--- src/app/portfolio-detail-page/portfolio-detail-page.component.ts
+++ src/app/portfolio-detail-page/portfolio-detail-page.component.ts
@@ -9,20 +9,51 @@ import { ApiService } from '../api.service';
 })
 export class PortfolioDetailPageComponent implements OnInit {
   public project: any;
+  public projectView = 0;
+  private key = 'projectViews';
+  private value = '1';
+  private currentRoute = this.route.snapshot.params['id'];
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+    this.updateProjectViews();
+    this.updateProjectsViewed();
+  }
 
   ngOnInit(): void {
     this.apiService.getData().subscribe((response) => {
-      console.log('from pdp');
       this.project = response.project;
 
       this.project.forEach((item: any) => {
-        if (item.project_dir === this.route.snapshot.params['id']) {
+        if (item.project_dir === this.currentRoute) {
           this.project = item;
           console.log(this.project);
         }
       });
     });
+  }
+
+  updateProjectViews(): void {
+    const key = 'projectViews';
+    let iteratedViews: number;
+
+    const currentViews = sessionStorage.getItem(key) || '0';
+    iteratedViews = parseInt(currentViews) + 1;
+    sessionStorage.setItem(key, iteratedViews.toString());
+  }
+
+  updateProjectsViewed(): void {
+    const key = 'projectsViewed';
+    let currentProjectRoute: any = [];
+    const lastViewed = sessionStorage.getItem(key);
+
+    if (lastViewed) {
+      currentProjectRoute = lastViewed.split(',');
+    }
+
+    // check if route already exists as last item
+    if (currentProjectRoute.slice(-1).indexOf(this.currentRoute) === -1) {
+      currentProjectRoute.unshift(this.currentRoute);
+    }
+    sessionStorage.setItem(key, currentProjectRoute.toString());
   }
 }
