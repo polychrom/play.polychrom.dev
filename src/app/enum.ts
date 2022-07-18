b2ac93d5e61a13fed280515c4e14d114983d327a
polychrom <poly.chrom@outlook.com>
2022-07-18 23:28:24 +0200

    added grid view

--- src/app/enum.ts
+++ src/app/enum.ts
@@ -3,4 +3,5 @@ export enum Enum {}
 export enum View {
   Index = 'Index',
   Default = 'Default',
+  Grid = 'Grid',
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
 import { View } from '../enum';
+import { distinctUntilChanged, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -8,18 +9,31 @@ import { View } from '../enum';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  public galleryMode = 'Index';
+  readonly View = View;
+  public galleryMode = View.Index;
+
+  public currentMode: any;
 
   constructor(public sharedService: SharedService) {}
 
   ngOnInit(): void {}
-  toggleView() {
-    if (this.galleryMode === View.Default) {
+
+  nextViewMode(): void {
+    this.sharedService.$galleryMode.pipe(take(1)).subscribe((mode) => {
+      this.currentMode = mode;
+    });
+
+    if (this.currentMode === View.Default) {
+      this.sharedService.$galleryMode.next(View.Index);
+      this.galleryMode = View.Grid;
+    }
+    if (this.currentMode === View.Index) {
+      this.sharedService.$galleryMode.next(View.Grid);
+      this.galleryMode = View.Default;
+    }
+    if (this.currentMode === View.Grid) {
       this.sharedService.$galleryMode.next(View.Default);
       this.galleryMode = View.Index;
-    } else {
-      this.sharedService.$galleryMode.next(View.Index);
-      this.galleryMode = View.Default;
     }
   }
 }
