1ec12717d0934ff0f2d5880503e3023f350c0931
polychrom <poly.chrom@outlook.com>
2022-07-21 22:18:45 +0200

    added viewed feature

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChildren,
+} from '@angular/core';
 import { ApiService } from '../api.service';
 import { SharedService } from '../shared.service';
 import { View } from '../enum';
-import { ActivatedRoute } from '@angular/router';
-import { fromEvent } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { BehaviorSubject, fromEvent } from 'rxjs';
 import { HelperService } from '../helper.service';
 
 @Component({
@@ -18,6 +23,10 @@ export class GalleryComponent implements OnInit {
   public posX: any;
   public posY: any;
   private _lastId: any;
+  public viewed: any = [];
+  public _routeMatch = false;
+  public seen: any;
+  public viewedFromStorage: any;
 
   viewportScroller: any;
   private column = {
@@ -28,11 +37,15 @@ export class GalleryComponent implements OnInit {
   public data: any;
   public projects: any;
 
+  public $items = new BehaviorSubject(['test']);
+
   constructor(
     private apiService: ApiService,
     private sharedService: SharedService,
     private activatedRoute: ActivatedRoute,
-    private _helperService: HelperService
+    private _helperService: HelperService,
+    private _router: Router,
+    public cdr: ChangeDetectorRef
   ) {
     // restore gallery view when reloading app
     const viewModeFromQuery = this.activatedRoute.snapshot.queryParams['view'];
@@ -42,6 +55,10 @@ export class GalleryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (localStorage.getItem('viewed')) {
+      this.seen = JSON.parse(localStorage.getItem('viewed') || 'nix');
+    }
+
     this.apiService.$data.subscribe((res: any) => {
       this.projects = res.project;
     });
@@ -49,6 +66,43 @@ export class GalleryComponent implements OnInit {
     this.sharedService.$galleryMode.subscribe((currentMode) => {
       this.galleryMode = currentMode;
     });
+
+    if (this._helperService.isBrowser()) {
+      this._router.events.subscribe((event: any) => {
+        if (event instanceof NavigationEnd) {
+          let currentProject = this._router.routerState.snapshot.url;
+          currentProject = currentProject.replace('/', '');
+
+          //let viewed: any;
+
+          // check if route is candidate for project match
+          this._routeMatch = false;
+          this.projects.forEach((project: any) => {
+            if (currentProject.indexOf(project.title) > -1) {
+              this._routeMatch = true;
+            }
+          });
+          // check if storage exists and project is already stored
+          if (this._routeMatch) {
+            if (localStorage.getItem('viewed')) {
+              this.viewedFromStorage = JSON.parse(
+                localStorage.getItem('viewed') || ''
+              );
+            }
+
+            // push item when storage does not exist or item not already saved
+            if (
+              !this.viewedFromStorage ||
+              (this.viewedFromStorage &&
+                !this.isInStorage(this.viewedFromStorage, currentProject))
+            ) {
+              this.viewed.push(currentProject);
+              localStorage.setItem('viewed', JSON.stringify(this.viewed));
+            }
+          }
+        }
+      });
+    }
 
     if (this._helperService.isBrowser()) {
       const mouseMove$ = fromEvent(window, 'mousemove');
@@ -76,6 +130,17 @@ export class GalleryComponent implements OnInit {
         }
       });
     }
+  }
+
+  isInStorage(viewed: any, current: string): boolean {
+    let _found = false;
+
+    viewed.forEach((element: any) => {
+      if (element === current) {
+        _found = true;
+      }
+    });
+    return _found;
   }
 
   getSpan(width: number): string {
